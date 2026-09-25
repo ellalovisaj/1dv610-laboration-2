80528195bd76ece2ac12c33932e98b601fb621e7
Ella-Lovisa Johansson <ej224xq@student.lnu.com>
2026-09-25 16:12:53 +0200

    Setup main module with functions

---
+++ src/Holiday.js
@@ -0,0 +1,49 @@
+/**
+ * Module...
+ */
+export class Holiday {
+  #year
+
+  /**
+   * Constructor.
+   *
+   * @param year
+   */
+  constructor(year) {
+    this.#year = year
+  }
+
+  /**
+   * Checks if date is a holiday.
+   *
+   * @param date
+   */
+  isHoliday(date) {
+    // TODO: test code
+    if (date === '2026-12-25') {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  /**
+   *
+   */
+  // findNextHoliday() {}
+
+  /**
+   *
+   */
+  // workdaysUntil() {}
+
+  /**
+   *
+   */
+  // workdaysBetween() {}
+
+  /**
+   *
+   */
+  // holidaysLeftThisYear() {}
+}
