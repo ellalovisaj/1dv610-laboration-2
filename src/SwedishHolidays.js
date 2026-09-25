b27b24e131c0bc7d1c67460c0c238a2b06efc88d
Ella-Lovisa Johansson <ej224xq@student.lnu.com>
2026-09-25 17:24:37 +0200

    Add functions to calculate what days the holidays falls on

--- src/SwedishHolidays.js
+++ src/SwedishHolidays.js
@@ -3,7 +3,7 @@
  */
 
 // All fixed holidays, the same date every year
-const fixedHolidays = [
+export const fixedHolidays = [
   { name: 'New Years Day', month: 1, day: 1 },
   { name: 'Epiphany', month: 1, day: 6 },
   { name: 'First of May', month: 5, day: 1 },
@@ -29,6 +29,33 @@ const fixedHolidays = [
 // Pentecost
 // 49 days after Easter
 
+// Midsummer
+// Day after Friday that falls between 19 and 25 of June
+/**
+ *
+ * @param year
+ */
+function getMidsummerDayDate(year) {
+  // calculate here
+  // return date
+}
 
 // All Saint's Day
 // First Saturday between October 31 and November 6
+/**
+ *
+ * @param year
+ */
+function getAllSaintsDayDate(year) {
+  // calculate here
+  // return date
+}
+
+/**
+ *
+ * @param date
+ */
+function getWeekday(date) {
+  const weekday = new Date(date).getDay()
+  return weekday
+}
